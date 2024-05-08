a21276520ef7ab1c46d02fb4bb2b9b3db2c99d29
Joseph Beck <joebeck03@icloud.com>
2024-05-08 20:46:10 +0100

    feat: add value map to ctx

--- pkg/amp/ctx.go
+++ pkg/amp/ctx.go
@@ -1,6 +1,7 @@
 package amp
 
 import (
+	"errors"
 	"net/http"
 	"sync"
 )
@@ -11,4 +12,25 @@ type Ctx struct {
 
 	values   map[string]any
 	valuesMu sync.Mutex
+}
+
+// Set a value in the Ctx values map.
+func (ctx *Ctx) Set(key string, val any) {
+	ctx.valuesMu.Lock()
+	defer ctx.valuesMu.Unlock()
+
+	ctx.values[key] = val
+}
+
+// Get a value from the Ctx values map.
+func (ctx *Ctx) Get(key string) (any, error) {
+	ctx.valuesMu.Lock()
+	defer ctx.valuesMu.Unlock()
+
+	val, ok := ctx.values[key]
+	if !ok {
+		return nil, errors.New("error, could not find value in map")
+	}
+
+	return val, nil
 }
--- pkg/amp/ctx_test.go
+++ pkg/amp/ctx_test.go
@@ -1,1 +1,34 @@
 package amp
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func newCtx() Ctx {
+	return Ctx{
+		values:   make(map[string]any),
+		valuesMu: sync.Mutex{},
+	}
+}
+
+func TestCtxSet(t *testing.T) {
+	ctx := newCtx()
+
+	ctx.Set("key", "value")
+	assert.Equal(t, ctx.values["key"], "value")
+}
+
+func TestCtxGet(t *testing.T) {
+	ctx := newCtx()
+
+	ctx.Set("key", "value")
+	val, err := ctx.Get("key")
+	assert.NoError(t, err)
+	assert.Equal(t, "value", val)
+
+	_, err = ctx.Get("no-key")
+	assert.Error(t, err)
+}
