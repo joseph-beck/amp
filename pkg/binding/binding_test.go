e2d2cf39a787a534d9cd9e12eb07cd2c3e8f9bfc
Joseph Beck <joebeck03@icloud.com>
2024-05-11 00:40:50 +0100

    test: struct validator

--- pkg/binding/binding_test.go
+++ pkg/binding/binding_test.go
@@ -1,1 +1,55 @@
 package binding
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Mock struct {
+	Key string `binding:"required"`
+}
+
+func TestReadBody(t *testing.T) {
+
+}
+
+func TestValidateStruct(t *testing.T) {
+	// test validator
+
+	validator := &defaultValidator{}
+
+	err := validator.ValidateStruct(Mock{Key: "value"})
+	assert.NoError(t, err)
+
+	err = validator.ValidateStruct(Mock{})
+	assert.Error(t, err)
+
+	// test nil validator
+
+	validator = &defaultValidator{}
+	err = validator.ValidateStruct(nil)
+	assert.NoError(t, err)
+
+	// test arrays
+
+	validator = &defaultValidator{}
+
+	err = validator.ValidateStruct([]Mock{})
+	assert.NoError(t, err)
+
+	err = validator.ValidateStruct([]Mock{{}, {Key: "value"}})
+	assert.Error(t, err)
+
+	validationError, ok := err.(SliceValidationError)
+	assert.True(t, ok)
+	assert.Len(t, validationError, 1)
+
+	assert.Error(t, validationError[0])
+}
+
+func TestEngine(t *testing.T) {
+	validator := &defaultValidator{}
+	engine := validator.Engine()
+	assert.NotNil(t, engine)
+}
