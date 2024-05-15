d6b69205ca3bb2c76d4cf5379e178e5fb7ef6a1e
Joseph Beck <joebeck03@icloud.com>
2024-05-15 20:00:48 +0100

    feat: add toml example

--- examples/render/main.go
+++ examples/render/main.go
@@ -8,20 +8,28 @@ import (
 )
 
 type Mock struct {
-	Value string `json:"value"`
+	Value string `json:"value" toml:"value"`
 }
 
-func getMock() amp.Handler {
+func getJSON() amp.Handler {
 	return func(ctx *amp.Ctx) error {
-		val := Mock{Value: "hello"}
+		val := Mock{Value: "json"}
 		return ctx.RenderJSON(status.OK, val)
+	}
+}
+
+func getTOML() amp.Handler {
+	return func(ctx *amp.Ctx) error {
+		val := Mock{Value: "json"}
+		return ctx.RenderTOML(status.OK, val)
 	}
 }
 
 func main() {
 	a := amp.New()
 
-	a.Get("/mock", getMock())
+	a.Get("/json", getJSON())
+	a.Get("/toml", getTOML())
 
 	log.Fatalln(a.ListenAndServe())
 }
