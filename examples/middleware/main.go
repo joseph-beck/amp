74021a53597e2aceb2c25eb9ee693d9220a17aec
Joseph Beck <joebeck03@icloud.com>
2024-06-15 20:24:49 +0100

    feat: restart cors, go 1.22 mux doesn't like it :/

--- examples/middleware/main.go
+++ examples/middleware/main.go
@@ -5,7 +5,6 @@ import (
 	"log"
 
 	"github.com/joseph-beck/amp/pkg/amp"
-	"github.com/joseph-beck/amp/pkg/middleware/cors"
 	"github.com/joseph-beck/amp/pkg/status"
 )
 
@@ -21,6 +20,7 @@ func getHandler() amp.Handler {
 	return func(ctx *amp.Ctx) error {
 		fmt.Println("handler")
 
+		ctx.Status(status.OK)
 		return nil
 	}
 }
@@ -39,15 +39,6 @@ func postHandler() amp.Handler {
 
 func main() {
 	a := amp.New()
-
-	a.Use(cors.New(cors.Config{
-		AllowedOrigins:   []string{"*", "http://localhost:5173", "http://127.0.0.1:5173"},
-		AllowedMethods:   []string{"GET", "POST"},
-		AllowedHeaders:   []string{"Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "accept", "origin", "Cache-Control", "X-Requested-With"},
-		AllowCredentials: true,
-		ExposeHeaders:    []string{},
-		Debug:            true,
-	}))
 
 	a.Get("/", getHandler(), middleware())
 
--- pkg/middleware/cors/cfg.go
+++ pkg/middleware/cors/cfg.go
@@ -1,10 +1,1 @@
 package cors
-
-type Config struct {
-	AllowedOrigins   []string
-	AllowedMethods   []string
-	AllowedHeaders   []string
-	AllowCredentials bool
-	ExposeHeaders    []string
-	Debug            bool
-}
